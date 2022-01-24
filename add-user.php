3c23bb216b729d360c79650832cc82f21410010b
chalanicodes <chalanicodes@gmail.com>
2022-01-24 23:31:33 +1100

    Added form validation for add new user

--- add-user.php
+++ add-user.php
@@ -1,6 +1,42 @@
 <?php session_start(); ?>
 <?php require_once('inc/connection.php'); ?>
 <?php require_once('inc/functions.php'); ?>
+<?php
+
+    $errors = array();
+
+    if (isset($_POST['submit'])) {
+
+        // checking requied fields
+        $req_fields = array('first_name' , 'last_name' , 'email' , 'password');
+
+        foreach ($req_fields as $field){
+            if (empty(trim($_POST[$field]))) {
+                $errors[] = $field . ' is required';
+            }    
+        }
+
+        // checking max length
+        $max_len_fields = array('first_name' => 50, 'last_name' => 100, 'email' => 100, 'password' => 40);
+
+        foreach ($max_len_fields as $field => $max_len) {
+            if (strlen(trim($_POST[$field])) > $max_len) {
+                $errors[] = $field . ' must be less than ' . $max_len . ' characters ';
+            }    
+        }
+
+        //checking email address
+         
+        if(!is_email($_POST['email'])){
+            $errors [] = 'Email address is invalid.';
+
+        }
+
+
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,26 +52,39 @@
     <main>
         <h1>Add New User<span><a href="users.php"> < Back to User List</a></span></h1>
 
+        <?php 
+        
+            if (!empty($errors)) {
+                echo '<div class="errmsg">';
+                echo '<b>There were error(s) on your form.</b><br>'; 
+                foreach ($errors as $error) {
+                    echo $error . '<br>';
+                }
+                echo '</div>';
+            }
+        
+         ?>
+
         <form action="add-user.php" method="POST" class="userform">
 
             <p>
                 <label for=""> First Name: </label>
-                <input type="text" name="first_name" required>
+                <input type="text" name="first_name">
             </p>
 
             <p>
                 <label for=""> Last Name: </label>
-                <input type="text" name="last_name" required>
+                <input type="text" name="last_name">
             </p>
 
             <p>
                 <label for=""> Email Address: </label>
-                <input type="email" name="email" required>
+                <input type="email" name="email">
             </p>
 
             <p>
                 <label for=""> New Password: </label>
-                <input type="password" name="password"required>
+                <input type="password" name="password">
             </p>
 
             <p>
